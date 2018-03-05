7f1e91ef2830a6cb0121008165a128765a19535d
savvala <savva.l@hotmail.com>
2018-03-05 15:47:46 +0000

    election.sol

---
+++ contracts/Election.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.4.2;
+
+contract Election {
+  // model a Candidate
+  struct Candidate {
+  uint id;
+  string name;
+  uint voteCount;
+}
+  // Store Candidate
+  // Fetch Candidate
+  mapping(uint => Candidate) public candidates;
+  // Store Candidate's Count
+  uint public candidatesCount;
+
+  function Election () public {
+    addCandidate("Candidate1");
+    addCandidate("Candidate2");
+  }
+
+  function addCandidate (string _name) private {
+    candidatesCount ++;
+    candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+  }
+}
