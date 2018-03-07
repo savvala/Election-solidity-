a032469d21248b33791731c684f7b4a695cb8fa5
savvala <savva.l@hotmail.com>
2018-03-07 16:02:35 +0000

    added requirements

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -26,6 +26,11 @@ contract Election {
   }
 
   function vote (uint _candidateId) public {
+    // require that they haven't voted before
+    require(!voters[msg.sender]);
+    // require a valid candidate
+    require(_candidateId > 0 && _candidateId <= candidatesCount);
+
     // record that voter has voted
     voters[msg.sender] = true;
 
