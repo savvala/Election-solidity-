9babc8ebdd05a1414f660bcaf80e7448132856c0
savvala <savva.l@hotmail.com>
2018-03-07 15:47:57 +0000

    voting test

--- test/election.js
+++ test/election.js
@@ -1,27 +1,42 @@
-var Election = artifacts.require("./Election.sol");
+var Election = artifacts.require('./Election.sol');
 
-contract("Election", function(accounts) {
+contract('Election', function(accounts) {
 
-  it("initializes with two candidates", function() {
+  it('initializes with two candidates', function() {
     return Election.deployed().then(function(instance) {
       return instance.candidatesCount();
     }).then(function(count) {
       assert.equal(count, 2);
     });
   });
-  it("it initializes the candidates with the correct values", function() {
+  it('it initializes the candidates with the correct values', function() {
     return Election.deployed().then(function(instance) {
       electionInstance = instance;
       return electionInstance.candidates(1);
     }).then (function(candidate) {
-      assert.equal(candidate[0], 1, "contains the correct id");
-      assert.equal(candidate[1], "Candidate 1", "contains the correct name");
-      assert.equal(candidate[2], 0, "contains the correct votes count");
+      assert.equal(candidate[0], 1, 'contains the correct id');
+      assert.equal(candidate[1], 'Candidate 1', 'contains the correct name');
+      assert.equal(candidate[2], 0, 'contains the correct votes count');
       return electionInstance.candidates(2);
     }).then(function(candidate) {
-      assert.equal(candidate[0], 2, "contains the correct id");
-      assert.equal(candidate[1], "Candidate 2", "contains the correct name")
-      assert.equal(candidate[2], 0, "contains the correct votes count");
+      assert.equal(candidate[0], 2, 'contains the correct id');
+      assert.equal(candidate[1], 'Candidate 2', 'contains the correct name');
+      assert.equal(candidate[2], 0, 'contains the correct votes count');
     });
+  });
+  it('allows a voter to cast a vote', function() {
+    return Election.deployed().then(function(instance) {
+      electionInstance = instance;
+      candidateId = 1;
+      return electionInstance.vote(candidateId, { from: accounts[0] });
+    }).then(function(receipt) {
+      return electionInstance.voters(accounts[0]);
+    }).then(function(voted) {
+      assert(voted, 'the voter was marked as voted');
+      return electionInstance.candidates(candidateId);
+    }).then(function(candidate) {
+      var voteCount = candidate[2];
+      assert.equal(voteCount, 1, 'increments the candidates vote count');
+    })
   });
 });
